5b923e44c8bd6f4051488a1778baf7409f64eb7c
F4de78 <casazucca10@gmail.com>
2024-03-07 19:33:24 +0100

    reduced max error, but still too high

--- CUDA/heat.cu
+++ CUDA/heat.cu
@@ -5,74 +5,63 @@
 // Simple define to index into a 1D array from 2D space
 #define I2D(row_len, c, r) ((r) * (row_len) + (c))
 
-#define BLOCKS 256
-#define THREADS 512
-
-__global__ void step_kernel_mod(int ni, int nj, float fact, float *temp_in, float *temp_out)
-{
-  int i00, im10, ip10, i0m1, i0p1;
-  float d2tdx2, d2tdy2;
-
-  int j = threadIdx.x + blockDim.x * blockIdx.x;
-  int i = threadIdx.y + blockDim.y * blockIdx.y;
-
-  if(i>=ni-1 || j>=nj-1){
-    printf("altro bound %d %d\n",i,j );
-    return;
-  }
-  if(i==0 || j==0){
-    printf("bound %d %d\n",i,j );
-    return;
-  }
-
-  printf("i:%d j:%d\n", i, j);
-  // find indices into linear memory
-  // for central point and neighbours
-  i00 = I2D(ni, i, j);
-  im10 = I2D(ni, i - 1, j);
-  ip10 = I2D(ni, i + 1, j);
-  i0m1 = I2D(ni, i, j - 1);
-  i0p1 = I2D(ni, i, j + 1);
-
-  // evaluate derivatives
-  d2tdx2 = temp_in[im10] - 2 * temp_in[i00] + temp_in[ip10];
-  d2tdy2 = temp_in[i0m1] - 2 * temp_in[i00] + temp_in[i0p1];
-
-  // update temperatures
-  temp_out[i00] = temp_in[i00] + fact * (d2tdx2 + d2tdy2);
-
-
-  //  int i00, im10, ip10, i0m1, i0p1;
+//__global__ void step_kernel_mod(int ni, int nj, float fact, float *temp_in, float *temp_out)
+//{
+  // int i00, im10, ip10, i0m1, i0p1;
   // float d2tdx2, d2tdy2;
 
-  // // loop over all points in domain (except boundary)
-  // for (int j = 1; j < nj - 1; j++)
-  // {
-  //   for (int i = 1; i < ni - 1; i++)
-  //   {
-  //     // find indices into linear memory
-  //     // for central point and neighbours
-  //     i00 = I2D(ni, i, j);
-  //     im10 = I2D(ni, i - 1, j);
-  //     ip10 = I2D(ni, i + 1, j);
-  //     i0m1 = I2D(ni, i, j - 1);
-  //     i0p1 = I2D(ni, i, j + 1);
+  // int j = threadIdx.x + blockDim.x * blockIdx.x;
+  // int i = threadIdx.y + blockDim.y * blockIdx.y;
 
-  //     // evaluate derivatives
-  //     d2tdx2 = temp_in[im10] - 2 * temp_in[i00] + temp_in[ip10];
-  //     d2tdy2 = temp_in[i0m1] - 2 * temp_in[i00] + temp_in[i0p1];
-
-  //     // update temperatures
-  //     temp_out[i00] = temp_in[i00] + fact * (d2tdx2 + d2tdy2);
-  //   }
+  // if(i>=ni-1 || j>=nj-1){
+  //   printf("altro bound %d %d\n",i,j );
+  //   return;
   // }
-}
+  // if(i==0 || j==0){
+  //   printf("bound %d %d\n",i,j );
+  //   return;
+  // }
+
+  // printf("i:%d j:%d\n", i, j);
+  // // find indices into linear memory
+  // // for central point and neighbours
+  // i00 = I2D(ni, i, j);
+  // im10 = I2D(ni, i - 1, j);
+  // ip10 = I2D(ni, i + 1, j);
+  // i0m1 = I2D(ni, i, j - 1);
+  // i0p1 = I2D(ni, i, j + 1);
+
+  // // evaluate derivatives
+  // d2tdx2 = temp_in[im10] - 2 * temp_in[i00] + temp_in[ip10];
+  // d2tdy2 = temp_in[i0m1] - 2 * temp_in[i00] + temp_in[i0p1];
+
+  // // update temperatures
+  // temp_out[i00] = temp_in[i00] + fact * (d2tdx2 + d2tdy2);
+
+
+  __global__ void step_kernel_mod(int ni, int nj, float fact, float *temp_in, float *temp_out) {
+      int j = threadIdx.x + blockDim.x * blockIdx.x;
+      int i = threadIdx.y + blockDim.y * blockIdx.y;
+
+      if (i >= 1 && i < ni - 1 && j >= 1 && j < nj - 1) {
+          int i00 = I2D(ni, i, j);
+          int im10 = I2D(ni, i - 1, j);
+          int ip10 = I2D(ni, i + 1, j);
+          int i0m1 = I2D(ni, i, j - 1);
+          int i0p1 = I2D(ni, i, j + 1);
+
+          float d2tdx2 = temp_in[im10] - 2 * temp_in[i00] + temp_in[ip10];
+          float d2tdy2 = temp_in[i0m1] - 2 * temp_in[i00] + temp_in[i0p1];
+
+          temp_out[i00] = temp_in[i00] + fact * (d2tdx2 + d2tdy2);
+      }
+  }
 
 void step_kernel_ref(int ni, int nj, float fact, float *temp_in, float *temp_out)
 {
   int i00, im10, ip10, i0m1, i0p1;
   float d2tdx2, d2tdy2;
-  printf("%d %d\n", ni, nj);
+  //printf("%d %d\n", ni, nj);
   // loop over all points in domain (except boundary)
   for (int j = 1; j < nj - 1; j++)
   {
@@ -102,8 +91,8 @@ int main()
   int nstep = 200; // number of time steps
 
   // Specify our 2D dimensions
-  const int ni = 10;
-  const int nj = 10;
+  const int ni = 1000;
+  const int nj = 1000;
   float tfac = 8.418e-5; // thermal diffusivity of silver
 
   float *temp1_ref, *temp2_ref, *temp1, *temp2, *temp_tmp, *cpu_arr;
@@ -144,16 +133,23 @@ int main()
   }
 
   // Execute the modified version using same data
+
+  // https://developer.nvidia.com/blog/cuda-refresher-cuda-programming-model/
+  
+  dim3 threadsPerBlock(32, 32); // 1024 threads per block
+  dim3 numBlocks((ni + threadsPerBlock.x - 1) / threadsPerBlock.x, (nj + threadsPerBlock.y - 1) / threadsPerBlock.y);
+  
   for (istep = 0; istep < nstep; istep++)
   {
-    step_kernel_mod<<<BLOCKS, THREADS>>>(ni, nj, tfac, temp1, temp2);
-
+    step_kernel_mod<<<numBlocks, threadsPerBlock>>>(ni, nj, tfac, temp1, temp2);
+    cudaDeviceSynchronize();
     // swap the temperature pointers
     // cudaMemcpy(temp1, temp2, size, cudaMemcpyDeviceToHost);
     temp_tmp = temp1;
     temp1 = temp2;
     temp2 = temp_tmp;
     //cudaMemcpy(temp1, temp2, size, cudaMemcpyDeviceToDevice);
+    
   }
   //cudaDeviceSynchronize();
 
