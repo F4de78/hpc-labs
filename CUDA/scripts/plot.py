be6091318028e341552fff61921d9a8d526b929c
filippo <fsiri2000@gmail.com>
2024-03-20 17:05:05 +0100

    update scripts for CUDA plots

--- CUDA/scripts/plot.py
+++ CUDA/scripts/plot.py
@@ -1,21 +1,40 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 sns.set_theme()
 
-def plot_heatmap(data):
+def plot_heatmap(data, n: int):
     fig, ax = plt.subplots(figsize=(10, 10))
-    data = data.pivot("thread_y", "thread_x", "average_time_gpu")
+    data = data[data['n'] == n]
+    data = data.pivot(index='thread_y', columns='thread_x', values='average_time_gpu')
     sns.heatmap(data, annot=True, fmt=".2f", linewidths=0, ax=ax, cmap="vlag")
-    ax.set_title("Heatmap of execution times")
+    ax.set_xlabel('#Thread per block (x)')
+    ax.set_ylabel('#Thread per block (y)')
+    ax.set_title(f"Heatmap of execution times for n = {n}")
     fig.savefig("report/img/heatmap.pdf")
 
-def plot_line(data):
+def plot_line(data, n: int):
+    data = data[data['n'] == n]
     fig, ax = plt.subplots(figsize=(10, 10))
     sns.lineplot(data=data, x="thread_x", y="average_time_gpu", hue="thread_y", ax=ax, marker="o", palette="tab10")
-    ax.set_title("Lineplot of execution times")
+    ax.set_title(f"Lineplot of execution times for n = {n}")
+    ax.set_xlabel('#Thread per block (x)')
+    ax.set_ylabel('Average time (ms)')
     fig.savefig("report/img/lineplot.pdf")
+
+def plot_line_3d(data, n: int):
+    data = data[data['n'] == n]
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(data['thread_x'], data['thread_y'], data['average_time_gpu'], cmap="vlag")
+    ax.set_xlabel('#Thread per block (x)')
+    ax.set_ylabel('#Thread per block (y)')
+    ax.set_zlabel('Average time (ms)')
+    ax.invert_yaxis()  # Reverse the order of the y-axis
+    ax.set_title(f"3D plot of execution times for n = {n}")
+    fig.savefig("report/img/lineplot3d.pdf")
 
 # plot a graph where n = 1000 and the x axis is pair (thread_x, thread_y) and the y axis is the average gpu time for the tuple (thread_x, thread_y)
 def plot_cpu_gpu(data, n: int):
@@ -29,7 +48,7 @@ def plot_cpu_gpu(data, n: int):
     plt.yscale('log')
     ax.set_xticks([1,2,4,8,16])
     ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: str((x,x))))
-    ax.set_ylabel("Average time (s)")
+    ax.set_ylabel("Average time (ms)")
     ax.set_xlabel("Thread number")
     ax.set_title(f"Lineplot of execution times for n = {n}")
     fig.savefig("report/img/cpu_gpu.pdf")
@@ -40,8 +59,9 @@ def main():
     data['average_time_cpu'] = data.groupby(['n'])['time_cpu'].transform('mean')
     data = data[['n', 'thread_x', 'thread_y', 'average_time_gpu', 'average_time_cpu']].drop_duplicates()
     print(data)
-    plot_heatmap(data)
-    plot_line(data)
+    plot_heatmap(data, 1000)
+    plot_line(data, 1000)
+    plot_line_3d(data, 1000)
     plot_cpu_gpu(data, 1000)
 
 
