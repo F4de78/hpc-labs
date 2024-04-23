f26920f074625720eb77275b8cb823835f91fbda
fedfontana <federico.fontana2000@gmail.com>
2024-04-23 15:54:17 +0200

    script stuff

--- mandelbrot/scripts/plot_cpu.py
+++ mandelbrot/scripts/plot_cpu.py
@@ -4,52 +4,86 @@ import matplotlib.pyplot as plt
 
 sns.set_theme()
 
+
 def plot_time(data):
     fig = plt.figure()
 
-    for n in data['res'].unique():
-        ax = sns.lineplot(data=data[data['res']==n], x="thread_no", y="average_time",label=n, marker='o', markers=True, dashes=False)
-    ax.set_xticks(data['thread_no'].unique())
+    for n in data["thread_no"].unique():
+        ax = sns.lineplot(
+            data=data[data["thread_no"] == n],
+            x="res",
+            y="average_time",
+            label=n,
+            marker="o",
+            markers=True,
+            dashes=False,
+        )
+    ax.set_xticks(data["res"].unique())
 
-    ax.set_ylabel("Average time (s)")
-    ax.set_xlabel("#Thread")
+    ax.set_ylabel("Average time (ms)")
+    ax.set_xlabel("Resolution")
     ax.set_title("Average execution time")
-    ax.legend(title="Resolution")
-    #plt.xscale('log')
-    fig.savefig("report/img_cpu/time.pdf") 
+    ax.legend(title="#Thread")
+    # plt.xscale('log')
+    fig.savefig("report/img_cpu/time.pdf")
+
 
 def plot_speedup(data):
     fig = plt.figure()
-    for n in data['res'].unique():
-        ax = sns.lineplot(data=data[data['res']==n], x="thread_no", y="speedup",label=n, marker='o', markers=True, dashes=False)
-    ax.set_xticks(data['thread_no'].unique())
+    for n in data["res"].unique():
+        ax = sns.lineplot(
+            data=data[data["res"] == n],
+            x="thread_no",
+            y="speedup",
+            label=n,
+            marker="o",
+            markers=True,
+            dashes=False,
+        )
+    ax.set_xticks(data["thread_no"].unique())
     ax.set_ylabel("Average speedup")
     ax.set_xlabel("#Thread")
     ax.set_title("Average speedup")
     ax.legend(title="Resolution")
-    #plt.xscale('log')
-    fig.savefig("report/img_cpu/speedup.pdf") 
+    # plt.xscale('log')
+    fig.savefig("report/img_cpu/speedup.pdf")
+
 
 def plot_efficiency(data):
     fig = plt.figure()
-    for n in data['res'].unique():
-        ax = sns.lineplot(data=data[data['res']==n], x="thread_no", y="efficiency",label=n, marker='o', markers=True, dashes=False)
-    ax.set_xticks(data['thread_no'].unique())
+    for n in data["res"].unique():
+        ax = sns.lineplot(
+            data=data[data["res"] == n],
+            x="thread_no",
+            y="efficiency",
+            label=n,
+            marker="o",
+            markers=True,
+            dashes=False,
+        )
+    ax.set_xticks(data["thread_no"].unique())
     ax.set_ylabel("Average efficiency")
     ax.set_xlabel("#Thread")
     ax.set_title("Average efficiency")
     ax.legend(title="Resolution")
-    #plt.xscale('log')
-    fig.savefig("report/img_cpu/efficiency.pdf") 
+    # plt.xscale('log')
+    fig.savefig("report/img_cpu/efficiency.pdf")
+
 
 def main():
-    data = pd.read_csv('report/data_cpu.csv')
+    data = pd.read_csv("report/data_cpu_vect.csv")
     # filter the optimal configuration
-    data = data[(data['ftype'] == 'float') & (data['fma'] == 'True') & (data['omp_scheduler'] == 'dynamic')]
-    data['average_time'] = data.groupby(['res', 'thread_no'])['time'].transform('mean')
-    data = data[['res', 'thread_no', 'average_time']].drop_duplicates()
-    data['speedup'] = data.apply(lambda row: data[(data['res'] == row['res']) & (data['thread_no'] == 1)]['average_time'].iloc[0] / row['average_time'], axis=1)
-    data['efficiency'] = data['speedup'] / data['thread_no']
+    print(data)
+    data = data[
+        (data["ftype"] == "float")
+        & (data["fma"] == True)
+        & (data["omp_scheduler"] == "dynamic")
+    ]
+    print(data)
+    data["average_time"] = data.groupby(["res", "thread_no"])["time"].transform("mean")
+    data = data[
+        ["res", "thread_no", "average_time", "speedup", "efficiency"]
+    ].drop_duplicates()
     print(data)
     plot_time(data)
     plot_speedup(data)
