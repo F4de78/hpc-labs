866df5f7a6c9491881536c3eefbc02ab6b7657d2
fedfontana <federico.fontana2000@gmail.com>
2024-05-06 15:51:53 +0200

    new image and gen old

--- mandelbrot/scripts/plot_cpu.py
+++ mandelbrot/scripts/plot_cpu.py
@@ -239,12 +239,69 @@ def plot_single_threaded_time_ratios(data):
     fig.savefig("report/img_cpu/time-single-thread-ratio.pdf")
 
 
+def plot_double_vs_float(float_data, double_data):
+    fig = plt.figure()
+
+    ax = sns.lineplot(
+        data=float_data[float_data["thread_no"] == 24],
+        x="res",
+        y="average_time",
+        label="float",
+        marker="o",
+        markers=True,
+        dashes=False,
+    )
+
+    ax = sns.lineplot(
+        data=double_data[double_data["thread_no"] == 24],
+        x="res",
+        y="average_time",
+        label="double",
+        marker="o",
+        markers=True,
+        dashes=False,
+    )
+    ax.set_xticks(float_data["res"].unique())
+
+    ax.set_ylabel("Average execution time")
+    ax.set_xlabel("Resolution")
+    ax.set_title("Average single threaded time ratio")
+    ax.legend(title="Code version")
+
+    fig.savefig("report/img_cpu/float_vs_double.pdf")
+
+
 def get_vect_data():
     data = pd.read_csv("report/data_cpu_vect.csv")
 
     # only keep the optimal configuration
     data = data[
         (data["ftype"] == "float")
+        & (data["fma"] == True)
+        & (data["omp_schedule"] == "dynamic")
+    ]
+
+    data["average_time"] = data.groupby(["res", "thread_no"])["time"].transform("mean")
+
+    data = data[["res", "thread_no", "average_time"]].drop_duplicates()
+
+    data["speedup"] = data.apply(
+        lambda row: data[(data["res"] == row["res"]) & (data["thread_no"] == 1)][
+            "average_time"
+        ].iloc[0]
+        / row["average_time"],
+        axis=1,
+    )
+    data["efficiency"] = data["speedup"] / data["thread_no"]
+    return data
+
+
+def get_double_vect_data():
+    data = pd.read_csv("report/data_cpu_vect.csv")
+
+    # only keep the optimal configuration
+    data = data[
+        (data["ftype"] == "double")
         & (data["fma"] == True)
         & (data["omp_schedule"] == "dynamic")
     ]
@@ -316,12 +373,15 @@ def main():
     data = get_data()
     vect_data = get_vect_data()
     vect_no_fma = get_vect_data_no_fma()
+    vect_double_data = get_double_vect_data()
 
     print("vect_data:\n", vect_data)
     plot_time(vect_data, fname="time-vect.pdf")
     plot_time_inv(vect_data)
     plot_speedup(vect_data, fname="speedup-vect.pdf")
     plot_efficiency(vect_data, fname="efficiency-vect.pdf")
+
+    plot_double_vs_float(vect_data, vect_double_data)
 
     plot_diff(data, vect_data)
     plot_vectorization_only_speedup(data, vect_data, vect_no_fma)
