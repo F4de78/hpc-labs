58fda2807ec4b494b05a5a6071306bd1f38e45f6
filippo <fsiri2000@gmail.com>
2024-05-07 17:07:43 +0200

    add fma

--- mandelbrot/scripts/table.py
+++ mandelbrot/scripts/table.py
@@ -39,9 +39,31 @@ def float_double(df):
 
     print(latex_table)
 
+def fma(df):
+    # Filter the DataFrame
+    filtered_df = df[(df['omp_schedule'] == 'dynamic') & (df['ftype'] == 'float')]
+
+    # Group by 'res', 'thread_no', and 'fma' and calculate the average of 'time'
+    grouped_df = filtered_df.groupby(['res', 'thread_no', 'fma'])['time'].mean().reset_index()
+
+    # Pivot the DataFrame
+    pivot_df = grouped_df.pivot_table(index='thread_no', columns=['res', 'fma'], values='time')
+
+    # Reset the index
+    pivot_df.reset_index(inplace=True)
+
+    # Convert the DataFrame to integers
+    pivot_df = pivot_df.astype(int)
+
+    # Convert the DataFrame to a LaTeX table
+    latex_table = pivot_df.to_latex(index=False, multirow=True)
+
+    print(latex_table)
+
 data_cpu = pd.read_csv('../report/data_cpu.csv')
 data_cpu_vect = pd.read_csv('../report/data_cpu_vect.csv')
 
 
 # static_dynamic(data_cpu)
-float_double(data_cpu_vect)
+#float_double(data_cpu_vect)
+fma(data_cpu_vect)
