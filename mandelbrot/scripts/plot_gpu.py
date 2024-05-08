9227be55ed113e8697dc84ed8f693abce54da37b
Marco Zucca <casazucca10@gmail.com>
2024-05-08 17:17:58 +0200

    plots

--- mandelbrot/scripts/plot_gpu.py
+++ mandelbrot/scripts/plot_gpu.py
@@ -25,17 +25,42 @@ def plot_line(data, n: int):
     ax.set_ylabel('Average time (ms)')
     fig.savefig(f"report/img_gpu/lineplot_{n}.pdf")
 
-# plot a graph where for every resolution on the x there is the pair (thread_x, thread_y) and on the y the average gpu time for the tuple (thread_x, thread_y)
-def plot_gpu_sqare(data):
-    fig, ax = plt.subplots(figsize=(20, 15))
-    data = data.pivot(index=['resolution', 'thread_x'], columns='thread_y', values='average_time_gpu').reset_index()
-    data = data.melt(id_vars=['resolution', 'thread_x'], var_name='thread_y', value_name='average_time_gpu')
-    sns.lineplot(data=data, x="thread_x", y="average_time_gpu", hue="resolution", ax=ax, marker="o", palette="tab10")
+# plot a lineplot where on the x axys  there are the tuple (x_thread,y_thread), on the y axys the average time for every resolution
+def plot_gpu_square(data):
+    fig, ax = plt.subplots(figsize=(10, 10))
+    data = data[data['thread_x'] == data['thread_y']]
+    # for every resolution plot the average time
+    for n in data['resolution'].unique():
+        data_n = data[data['resolution'] == n]
+        sns.lineplot(data=data_n, x="thread_x", y="average_time_gpu", ax=ax, marker="o", label=f"{n}")
+    ax.set_xticks([1,2,4,8,16])
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: str((x,x))))
+    # plt.yscale("log")
+    ax.set_ylabel("Average time (ms)")
+    ax.set_xlabel("Thread number")
     ax.set_title(f"Lineplot of execution times for every resolution")
-    ax.legend(title='resolution')
-    ax.set_xlabel('#Thread(x,y)')
-    ax.set_ylabel('Average time (ms)')
-    fig.savefig(f"report/img_gpu/lineplot_all.pdf")
+    fig.savefig(f"report/img_gpu/square.pdf")
+
+def plot_time(data):
+    fig = plt.figure(figsize=(10, 10))
+
+    data = data[data['thread_x'] == data['thread_y']]
+    for n in data["thread_x"].unique():
+        ax = sns.lineplot(
+            data=data[data["thread_x"] == n],
+            x="resolution",
+            y="average_time_gpu",
+            label=f"({n},{n})",
+            marker="o",
+            markers=True,
+            dashes=False,
+        )
+    ax.set_xticks(data["resolution"].unique())
+    ax.set_ylabel("Average time (ms)")
+    ax.set_xlabel("Resolution")
+    ax.set_title("Average execution time")
+    ax.legend(title="#Thread")
+    fig.savefig(f"report/img_gpu/gpu_time.pdf")
 
 
 def plot_line_3d(data, n: int):
@@ -57,10 +82,13 @@ def main():
     data['average_time_gpu'] = data.groupby(['resolution', 'thread_x', 'thread_y'])['time_gpu'].transform('mean')
     data = data[['resolution', 'thread_x', 'thread_y', 'average_time_gpu']].drop_duplicates()
     data.to_csv('report/data_graph_gpu.csv', index=False)
-    #for n in data['resolution'].unique():
-        # plot_heatmap(data, n)
-        # plot_line(data, n)
-    plot_gpu_sqare(data)
+
+
+    # for n in data['resolution'].unique():
+    #     plot_heatmap(data, n)
+    #     plot_line(data, n)
+    plot_time(data)
+    plot_gpu_square(data)
         # plot_line_3d(data, n)
 
 if __name__ == "__main__":
