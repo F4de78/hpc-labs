2b699aafe6e5c3d53e5d28d961aae70d716ac34e
fedfontana <federico.fontana2000@gmail.com>
2024-05-08 17:43:02 +0200

    added a print function and ran the script

--- mandelbrot/scripts/plot_gpu.py
+++ mandelbrot/scripts/plot_gpu.py
@@ -5,63 +5,114 @@ from mpl_toolkits.mplot3d import Axes3D
 
 sns.set_theme()
 
+
 def plot_heatmap(data, n: int):
     fig, ax = plt.subplots(figsize=(10, 10))
-    data = data[data['resolution'] == n]
-    data = data.pivot(index='thread_y', columns='thread_x', values='average_time_gpu')
+    data = data[data["resolution"] == n]
+    data = data.pivot(index="thread_y", columns="thread_x", values="average_time_gpu")
     sns.heatmap(data, annot=True, fmt=".2f", linewidths=0, ax=ax, cmap="magma_r")
-    ax.set_xlabel('#Thread per block (x)')
-    ax.set_ylabel('#Thread per block (y)')
+    ax.set_xlabel("#Thread per block (x)")
+    ax.set_ylabel("#Thread per block (y)")
     ax.set_title(f"Heatmap of execution times for n = {n}")
     fig.savefig(f"report/img_gpu/heatmap_{n}.pdf")
 
+
 def plot_line(data, n: int):
-    data = data[data['resolution'] == n]
+    data = data[data["resolution"] == n]
     fig, ax = plt.subplots(figsize=(10, 10))
-    sns.lineplot(data=data, x="thread_x", y="average_time_gpu", hue="thread_y", ax=ax, marker="o", palette="tab10")
+    sns.lineplot(
+        data=data,
+        x="thread_x",
+        y="average_time_gpu",
+        hue="thread_y",
+        ax=ax,
+        marker="o",
+        palette="tab10",
+    )
     ax.set_title(f"Lineplot of execution times for n = {n}")
-    ax.legend(title='#Thread per block (y)')
-    ax.set_xlabel('#Thread per block (x)')
-    ax.set_ylabel('Average time (ms)')
+    ax.legend(title="#Thread per block (y)")
+    ax.set_xlabel("#Thread per block (x)")
+    ax.set_ylabel("Average time (ms)")
     fig.savefig(f"report/img_gpu/lineplot_{n}.pdf")
+
 
 # plot a graph where for every resolution on the x there is the pair (thread_x, thread_y) and on the y the average gpu time for the tuple (thread_x, thread_y)
 def plot_gpu_sqare(data):
     fig, ax = plt.subplots(figsize=(20, 15))
-    data = data.pivot(index=['resolution', 'thread_x'], columns='thread_y', values='average_time_gpu').reset_index()
-    data = data.melt(id_vars=['resolution', 'thread_x'], var_name='thread_y', value_name='average_time_gpu')
-    sns.lineplot(data=data, x="thread_x", y="average_time_gpu", hue="resolution", ax=ax, marker="o", palette="tab10")
+    data = data.pivot(
+        index=["resolution", "thread_x"], columns="thread_y", values="average_time_gpu"
+    ).reset_index()
+    data = data.melt(
+        id_vars=["resolution", "thread_x"],
+        var_name="thread_y",
+        value_name="average_time_gpu",
+    )
+    sns.lineplot(
+        data=data,
+        x="thread_x",
+        y="average_time_gpu",
+        hue="resolution",
+        ax=ax,
+        marker="o",
+        palette="tab10",
+    )
     ax.set_title(f"Lineplot of execution times for every resolution")
-    ax.legend(title='resolution')
-    ax.set_xlabel('#Thread(x,y)')
-    ax.set_ylabel('Average time (ms)')
+    ax.legend(title="resolution")
+    ax.set_xlabel("#Thread(x,y)")
+    ax.set_ylabel("Average time (ms)")
     fig.savefig(f"report/img_gpu/lineplot_all.pdf")
 
 
 def plot_line_3d(data, n: int):
-    data = data[data['resolution'] == n]
+    data = data[data["resolution"] == n]
     fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_trisurf(data['thread_x'], data['thread_y'], data['average_time_gpu'], cmap="vlag")
-    ax.set_xlabel('#Thread per block (x)')
-    ax.set_ylabel('#Thread per block (y)')
-    ax.set_zlabel('Average time (ms)')
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_trisurf(
+        data["thread_x"], data["thread_y"], data["average_time_gpu"], cmap="vlag"
+    )
+    ax.set_xlabel("#Thread per block (x)")
+    ax.set_ylabel("#Thread per block (y)")
+    ax.set_zlabel("Average time (ms)")
     ax.invert_yaxis()  # Reverse the order of the y-axis
     ax.set_title(f"3D plot of execution times for n = {n}")
     fig.savefig(f"report/img_gpu/lineplot3d_{n}.pdf")
 
 
+def print_aggregate_table():
+    data = pd.read_csv("report/data_gpu.csv")
+
+    data = data[(data["thread_x"] == 16) & (data["thread_y"] == 16)][
+        ["resolution", "time_gpu", "ftype"]
+    ]
+
+    data["average_time"] = data.groupby(["resolution", "ftype"])["time_gpu"].transform(
+        "mean"
+    )
+
+    data = data[["resolution", "average_time", "ftype"]].drop_duplicates()
+
+    with pd.option_context("display.max_rows", None, "display.max_columns", None):
+        print(data)
+
+
 def main():
-    data = pd.read_csv('report/data_gpu.csv')
-    data = data[data['ftype'] == "double"]
-    data['average_time_gpu'] = data.groupby(['resolution', 'thread_x', 'thread_y'])['time_gpu'].transform('mean')
-    data = data[['resolution', 'thread_x', 'thread_y', 'average_time_gpu']].drop_duplicates()
-    data.to_csv('report/data_graph_gpu.csv', index=False)
-    #for n in data['resolution'].unique():
-        # plot_heatmap(data, n)
-        # plot_line(data, n)
+    data = pd.read_csv("report/data_gpu.csv")
+    data = data[data["ftype"] == "double"]
+    data["average_time_gpu"] = data.groupby(["resolution", "thread_x", "thread_y"])[
+        "time_gpu"
+    ].transform("mean")
+    data = data[
+        ["resolution", "thread_x", "thread_y", "average_time_gpu"]
+    ].drop_duplicates()
+    data.to_csv("report/data_graph_gpu.csv", index=False)
+    # for n in data['resolution'].unique():
+    # plot_heatmap(data, n)
+    # plot_line(data, n)
     plot_gpu_sqare(data)
-        # plot_line_3d(data, n)
+    # plot_line_3d(data, n)
+
+    print_aggregate_table()
+
 
 if __name__ == "__main__":
     main()
